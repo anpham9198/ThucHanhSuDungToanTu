8d326b1c3cdd0020bb1d51eb5933549fdbefa2e6
anpham9198 <anpham9198@gmail.com>
2024-10-05 02:33:27 +0700

    Add project files.

---
+++ ThucHanhSuDungToanTu/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float width;
+        float length;
+
+        Console.Write("Enter width: ");
+        width = float.Parse(Console.ReadLine());
+        Console.Write("Enter length: ");
+        length = float.Parse(Console.ReadLine());
+        
+        float area = width * length;
+        Console.WriteLine("Area is: " + area);
+
+        Console.ReadKey();
+    }
+}
